4af6f2bde03a391ea07c7185a3f672648fc4101a
Andrea Dashe <andrea.r.dashe@gmail.com>
2022-03-30 04:02:55 +0000

    Completed Part 1 and Part 2

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -3,7 +3,11 @@
 // PART 1: SHOW A FORTUNE
 
 function showFortune(evt) {
-  // TODO: get the fortune and show it in the #fortune-text div
+  fetch('/fortune')
+    .then(response => response.text())
+    .then(responseData => {
+      document.querySelector('#fortune-text').innerText = responseData;
+    });
 }
 
 document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
@@ -13,10 +17,14 @@ document.querySelector('#get-fortune-button').addEventListener('click', showFort
 function showWeather(evt) {
   evt.preventDefault();
 
-  const url = '/weather.json';
+  const url = "/weather.json";
   const zipcode = document.querySelector('#zipcode-field').value;
 
-  // TODO: request weather with that URL and show the forecast in #weather-info
+  fetch(`/weather.json?zipcode=${zipcode}`)
+    .then(response => response.json())
+    .then(responseData => {
+      document.querySelector('#weather-info').innerText = responseData.forecast;
+    });
 }
 
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
